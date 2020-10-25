663d14065771e740113fe1d93a94560ec71b21e1
Nuno <nunocorreia85@gmail.com>
2020-10-25 10:31:02 +0100

    trigger deploy

--- src/Api/Function1.cs
+++ src/Api/Function1.cs
@@ -17,7 +17,7 @@ namespace MyBills.Api
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request!");
 
             string name = req.Query["name"];
 
