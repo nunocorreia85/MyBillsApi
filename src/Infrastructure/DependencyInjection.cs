01fad47f59d334d95d9234ed80093db00abd9f7d
Nuno <nunocorreia85@gmail.com>
2020-11-09 04:53:10 +0100

    add serilog(console)

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using MyBills.Application.Common.Interfaces;
 using MyBills.Infrastructure.Persistence;
 using MyBills.Infrastructure.Services;
+using Serilog;
+using Serilog.Core;
 
 namespace MyBills.Infrastructure
 {
@@ -11,6 +13,12 @@ namespace MyBills.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services,
             IConfiguration configuration)
         {
+            // Registering Serilog provider
+            Logger logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(logger));
+            
             services.AddDbContext<IApplicationDbContext, ApplicationDbContext>();
 
             services.AddScoped<IDomainEventService, DomainEventService>();
