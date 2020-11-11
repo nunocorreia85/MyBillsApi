e0206e2e12ae457973238b93b25c63d842d20bec
Nuno <nunocorreia85@gmail.com>
2020-11-11 03:03:56 +0100

    revert upgrade

--- src/Application/Common/Mappings/IMapFrom.cs
+++ src/Application/Common/Mappings/IMapFrom.cs
@@ -3,7 +3,15 @@
 namespace MyBills.Application.Common.Mappings
 {
     public interface IMapFrom<T>
-    {   
-        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    {
+        void Mapping(Profile profile);
+    }
+
+    public abstract class MapFrom<T> : IMapFrom<T>
+    {
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap(typeof(T), GetType());
+        }
     }
 }
